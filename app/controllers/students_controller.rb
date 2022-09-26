7c384fd1c49404d7cdf26deb60f86ece99779bf3
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-09-26 09:09:45 +0300

    commit tree 6620cf773f5f7d20de7f84ee01bcd69ed45b7545

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,20 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  # query params in the index action
+  def index
+    students = Student.all
+    if params[:name]
+      students = students.select do |student|
+        student.first_name.downcase.include?(params[:name].downcase) || student.last_name.downcase.include?(params[:name].downcase)
+      end
+    end
+    render json: students
+  end
+
 end
